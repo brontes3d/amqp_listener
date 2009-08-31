12378eef449df984f14c9b9e93e1889453a2c860
Jacob Burkhart <jacob@brontes3d.com>
2009-08-31 18:00:57 -0400

    this alias method chain thing isn't working as I'd hoped, I'll have to make logging hooks directly in amqp gem

--- init.rb
+++ init.rb
@@ -1,14 +1,2 @@
 $:.unshift "#{File.dirname(__FILE__)}/lib"
 require 'amqp_listener'
-
-AMQP::Client.class_eval do
-  
-  def determine_reconnect_server_with_logging
-    try_host, try_port = determine_reconnect_server_without_logging
-    RAILS_DEFAULT_LOGGER.info("Connecting to #{try_host} #{try_port} (attempt #{@retry_count})")
-    return [try_host, try_port]
-  end
-  
-  alias_method_chain :determine_reconnect_server, :logging
-  
-end
